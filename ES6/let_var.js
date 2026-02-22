7f5fa56e9952d347f01e33270754f219577b992d
SK. Nur Alam <100152180+noor4964@users.noreply.github.com>
2026-02-23 02:34:25 +0600

    Let & var

---
+++ ES6/let_var.js
@@ -0,0 +1,26 @@
+// let and var keyword basically used in es6 is for scope
+// For Example:
+
+// first See var
+// Var is a global variable which value can be changed or you can say it can be modified after initialization
+
+var myName = "SK. NUR"
+console.log(myName);
+myName = "SK. NUR ALAM"
+console.log(myName);
+console.log("---------------------------------------------")
+// Now See let
+// let keyword is used to assign value which is already assigned(like var)
+// variable defined with let keyword is accessable within the block(loop, condition, function) it is declared.
+
+let wife = "Sadika";
+console.log(wife);
+wife = "Sadika Marium";
+console.log(wife);
+
+// Lastly const:
+// Used to define variable which cannot be reassigned.
+// Variable declared with the const can be accessed globally.
+console.log("---------------------------------------------");
+const sis = "Asia Akter";
+console.log(sis);

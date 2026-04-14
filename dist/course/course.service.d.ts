485345990a33959d4a006f186343f6c388407d29
SK. Nur Alam <100152180+noor4964@users.noreply.github.com>
2026-04-15 02:17:45 +0600

    Assignment-1

---
+++ dist/course/course.service.d.ts
@@ -0,0 +1,8 @@
+export declare class CourseService {
+    getAllCourses(): string;
+    getCourseById(id: number): string;
+    createCourse(): string;
+    updateCourse(id: number): string;
+    patchCourse(id: number): string;
+    deleteCourse(id: number): string;
+}

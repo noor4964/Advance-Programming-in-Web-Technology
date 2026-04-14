2125a751ffdb2f8385d61f481a68e60bbf687c6b
SK. Nur Alam <100152180+noor4964@users.noreply.github.com>
2026-04-15 02:24:00 +0600

    node_module removed

--- dist/course/course.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CourseService {
-    getAllCourses(): string;
-    getCourseById(id: number): string;
-    createCourse(): string;
-    updateCourse(id: number): string;
-    patchCourse(id: number): string;
-    deleteCourse(id: number): string;
-}
